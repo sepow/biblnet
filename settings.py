ed8fb503a9287d32d485b927a09708c96006bc2c
Sean Powell <BendtsenPowell@gmail.com>
2009-04-08 02:41:44 -0500

    Flyttede media/pinax ned til biblnet folder for the time beeing.

--- settings.py
+++ settings.py
@@ -50,7 +50,7 @@ USE_I18N = True
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
 
-MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "site_media")
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), "media")
 
 # URL that handles the media served from MEDIA_ROOT.
 # Example: "http://media.lawrence.com"
