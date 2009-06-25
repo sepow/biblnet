21de1c4acca2985de29053e7bbd529e5abcac38f
sean <sean@Malaz.(none)>
2009-06-25 15:31:24 +0200

    dato ting indsat i settings.py

--- settings.py
+++ settings.py
@@ -43,7 +43,7 @@ DATABASE_PORT = ''             # Set to empty string for default. Not used with 
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'Europe/Copenhagen'
-
+DATETIME_FORMAT = 'N j, Y, H:i'
 # Language code for this installation. All choices can be found here:
 # http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
 # http://blogs.law.harvard.edu/tech/stories/storyReader$15
