47f4a81ea1aedab6eeee0290f2e3c4348c3ad38c
sean <sean@Malaz.(none)>
2009-07-06 09:53:19 +0200

    fjernet private grupper fra affils

--- apps/profiles/forms.py
+++ apps/profiles/forms.py
@@ -1,7 +1,9 @@
 from django import forms
 from profiles.models import Profile
-
+from profiles.models import Affiliation
+from django.utils.translation import ugettext_lazy as _, ugettext
 class ProfileForm(forms.ModelForm):
+    affiliation = forms.ModelChoiceField(queryset=Affiliation.objects.filter(tribe__private__exact=False), empty_label=_("(Pick your affiliation)"))
     class Meta:
         model = Profile
         exclude = ('user', 'blogrss', 'timezone', 'language',
