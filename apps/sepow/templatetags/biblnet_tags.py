cbe753ea37109111e97832c3349004a6780891dd
Sean Bendtsen Powell <BendtsenPowell@gmail.com>
2009-08-27 17:55:54 +0200

    crazy fix

--- apps/sepow/templatetags/biblnet_tags.py
+++ apps/sepow/templatetags/biblnet_tags.py
@@ -124,11 +124,13 @@ register.inclusion_tag('sepow/latest_updates.html')(get_latest)
 
 @register.filter("truncate_chars")
 def truncate_chars(value, max_length):
+    '''
     if len(value) > max_length:
         truncd_val = value[:max_length]
         if value[max_length+1] != " ":
             truncd_val = truncd_val[:truncd_val.rfind(" ")]
         return  truncd_val + "..."
+    '''
     return value
 
 
