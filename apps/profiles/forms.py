b6d9d340708174b98d3836ce8463ea2b15045662
Luhr Løvdahl <luhr.dk@gmail.com>
2009-09-10 11:21:32 +0200

    Translation: Fixed translations in templates and locale files

--- apps/profiles/forms.py
+++ apps/profiles/forms.py
@@ -4,8 +4,8 @@ from profiles.models import Affiliation, Occupation
 from django.utils.translation import ugettext_lazy as _, ugettext
 
 class ProfileForm(forms.ModelForm):
-    affiliation = forms.ModelChoiceField(queryset=Affiliation.objects.filter(tribe__private__exact=False), empty_label=_("(Pick your affiliation)"))
-    occupation = forms.ModelChoiceField(queryset=Occupation.objects.all(), empty_label=_("(Pick your occupation)"))
+    affiliation = forms.ModelChoiceField(label=_(u"Affiliation"), queryset=Affiliation.objects.filter(tribe__private__exact=False), empty_label=_("(Pick your affiliation)"))
+    occupation = forms.ModelChoiceField(label=_(u"Occupation"), queryset=Occupation.objects.all(), empty_label=_("(Pick your occupation)"))
     class Meta:
         model = Profile
         exclude = ('user', 'blogrss', 'timezone',
