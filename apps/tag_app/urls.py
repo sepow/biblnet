fa4b7bcf654f6faf727fe9491318d17687752bf9
sean <sean@Malaz.(none)>
2009-10-12 10:00:58 +0200

    flyttet tagcloud logic fra sepow til tag_app

--- apps/tag_app/urls.py
+++ apps/tag_app/urls.py
@@ -3,6 +3,6 @@ from sepow.views import *
 
 urlpatterns = patterns('',
     # all tags
-    url(r'^$', 'sepow.views.show_tag_cloud', name='tag_cloud'),
+    url(r'^$', 'tag_app.views.show_tag_cloud', name='tag_cloud'),
     url(r'^(?P<tag>.+)/$', 'tag_app.views.tags', name='tag_results'),
 )
--- apps/tag_app/views.py
+++ apps/tag_app/views.py
@@ -13,6 +13,45 @@ from tribes.models import Topic as TribeTopic
 from django_dms.apps.small_dms.models import Document 
 from wiki.models import Article as WikiArticle
 
+from django.http import HttpResponseRedirect, HttpResponse, HttpResponseForbidden, Http404
+from math import log
+# thanks to http://dburke.info/blog/logarithmic-tag-clouds/
+from django.views.decorators.cache import cache_page
+from django.http import HttpResponseRedirect
+
+def tagcloud(threshold=1, maxsize=2.75, minsize=.70):
+    """usage: 
+        -threshold: Tag usage less than the threshold is excluded from
+            being displayed.  A value of 0 displays all tags.
+        -maxsize: max desired CSS font-size in em units
+        -minsize: min desired CSS font-size in em units
+    
+    Returns a list of dictionaries of the tag, its count and
+    calculated font-size.
+    """
+    counts, taglist, tagcloud = [], [], []
+    tags = Tag.objects.all()
+    if tags: 
+        for tag in tags:
+            count = tag.items.count()
+            count >= threshold and (counts.append(count), taglist.append(tag))
+        maxcount = max(counts)
+        mincount = min(counts)
+        constant = log(maxcount - (mincount - 1))/(maxsize - minsize) or 1
+        tagcount = zip(taglist, counts)
+        for tag, count in tagcount:
+            size = log(count - (mincount - 1))/constant + minsize
+            tagcloud.append({'tag': tag, 'count': count, 'size': round(size, 7)})
+        return tagcloud
+    return None
+@cache_page(60 * 60)
+def show_tag_cloud(request, template_name="sepow/tagcloud.html" ):
+    return render_to_response(template_name, {
+    'tag_cloud' : tagcloud()
+    }, context_instance=RequestContext(request)
+    )
+    
+
 def tags(request, tag, template_name='tags/index.html'):
     tag = get_object_or_404(Tag, name=tag)
     
@@ -38,3 +77,5 @@ def tags(request, tag, template_name='tags/index.html'):
         'wiki_article_tags': wiki_article_tags,
         'document_tags' : document_tags,
     }, context_instance=RequestContext(request))
+    
+
