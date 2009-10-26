e284057f00acd0011ee3878f978bb1bf0998d9ee
sean <sean@Malaz.(none)>
2009-10-26 14:07:19 +0100

    glemte denne her mappe

---
+++ apps/misc/views.py
@@ -0,0 +1,58 @@
+from django.http import HttpResponse
+from django.conf import settings
+
+# @@@ these can be cleaned up a lot, made more generic and with better queries
+
+def username_autocomplete_all(request):
+    if request.user.is_authenticated():
+        from django.contrib.auth.models import User
+        from basic_profiles.models import Profile
+        from avatar.templatetags.avatar_tags import avatar
+        q = request.GET.get("q")
+        users = User.objects.all()
+        content = []
+        # @@@ temporary hack -- don't try this at home (or on real sites)
+        for user in users:
+            if user.username.lower().startswith(q):
+                try:
+                    profile = user.get_profile()
+                    entry = "%s,,%s,,%s" % (
+                        avatar(user, 40),
+                        user.username,
+                        profile.location
+                    )
+                except Profile.DoesNotExist:
+                    pass
+                content.append(entry)
+        response = HttpResponse("\n".join(content))
+    else:
+        response = HttpResponseForbidden()
+    setattr(response, "djangologging.suppress_output", True)
+    return response
+
+
+def username_autocomplete_friends(request):
+    from django.utils import simplejson
+    from django.contrib.auth.models import User
+    from avatar.templatetags.avatar_tags import avatar
+    
+    if request.user.is_authenticated():
+        results = []
+        q = request.GET['q']
+        query = q.split(",")
+        
+        def create_entry(user):
+            return "%s,,%s,,%s" % ( avatar(user, 40), user.username, user.get_profile().name)
+        
+        if len(q) > 1:
+            users = User.objects.all().filter(is_active=True)
+            model_results = users.filter(profile__name__icontains=q) | users.filter(profile__user__username__icontains=q)
+            # model_results = User.objects.filter(username__istartswith=q).filter(is_active=True)[:10]
+            results = [create_entry(x) for x in model_results ] 
+        
+        response = HttpResponse("\n".join(results))
+    else:
+        response = HttpResponseForbidden()
+    
+    setattr(response, "djangologging.suppress_output", True)
+    return response
